9e909bae8b4a5fa724de4ef40fae1bbce9aa4c0d
Alex <alexcp.almeida@gmail.com>
2013-06-24 19:12:50 +0100

    remove folders

--- Dropbox/Projecto SEI/Entrega Final/Projecto SEI - Grupo 1/SeiLessons/HSN-Project/src/pt/saude365/entryregister/package-info.java
+++
@@ -1,6 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace =
-                                     "http://saude365.pt/registoentrada",
-                                     elementFormDefault =
-                                     javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package pt.saude365.entryregister;
-
--- Dropbox/Projecto SEI/Entrega Final/Projecto SEI - Grupo 1/SeiLessons/HSN-Project/src/pt/saude365/patientreport/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://saude365.pt",
-                                     elementFormDefault =
-                                     javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package pt.saude365.patientreport;
-
